e005570ef998f2af861a6484eaad14f102c70fc3
mithun <motyletsdenis@gmail.com>
2020-06-03 00:59:54 +0700

    django/...from docs ss

--- django/first_app_from_docs/polls/views.py
+++ django/first_app_from_docs/polls/views.py
@@ -1,43 +1,39 @@
+import io
 from django.db.models import F
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, FileResponse
+from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
-from django.utils import timezone
-from django.views import generic
+from django.conf import settings
+from reportlab.pdfgen import canvas
 from .models import Question, Choice
 
 
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
-
-    def get_queryset(self):
-        """
-        Return the last five published questions.
-        (not including those set to be published in the future)
-        """
-        return Question.objects.filter(
-            pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
+def detail(request, question_id):
+    #if not request.user.is_authenticated:
+        #return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
 
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'polls/results.html'
+def results(request, question_id):
+    #if not request.user.is_authenticated:
+        #return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
+    #if not request.user.is_authenticated:
+        #return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+
     question = get_object_or_404(Question, pk=question_id)
 
     try:
@@ -55,3 +51,27 @@ def vote(request, question_id):
             'polls:results',
             args=(question.id,)
         ))
+
+
+def docs(request):
+    #if not request.user.is_authenticated:
+        #return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
+
+    # Create a file-like buffer to receive PDF data.
+    buffer = io.BytesIO()
+
+    # Create the PDF object, using the buffer as its "file."
+    p = canvas.Canvas(buffer)
+
+    # Draw things on the PDF. Here's where the PDF generation happens.
+    # See the ReportLab documentation for the full list of functionality.
+    p.drawString(100, 100, "Hello world.")
+
+    # Close the PDF object cleanly, and we're done.
+    p.showPage()
+    p.save()
+
+    # FileResponse sets the Content-Disposition header so that browsers
+    # present the option to save the file.
+    buffer.seek(0)
+    return FileResponse(buffer, as_attachment=True, filename='hello.pdf')
