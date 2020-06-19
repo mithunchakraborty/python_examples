1b3bf615c10209929d18c7ca69d041a4d6926ff3
mithun <motyletsdenis@gmail.com>
2020-06-20 00:38:29 +0700

    add movi2e

--- django/movies/movies/views.py
+++ django/movies/movies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from .models import *
 
-# Create your views here.
+
+class MoviesView(View):
+    @staticmethod
+    def get(request):
+        movies = Movie.objects.all()
+        return render(request, 'movies/movies.html', {'movie_list': movies})
